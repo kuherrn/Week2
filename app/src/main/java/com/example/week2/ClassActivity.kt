233fc1d05751f2fea0ab8bb69bcfa1fcf8a3beb5
kuherrn <kuherrn@gmail.com>
2023-09-15 10:30:13 -0400

    EOC

--- app/src/main/java/com/example/week2/ClassActivity.kt
+++ app/src/main/java/com/example/week2/ClassActivity.kt
@@ -2,16 +2,15 @@ package com.example.week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.week2.databinding.ActivityClassBinding
 import com.example.week2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class ClassActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityClassBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityClassBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
     }
 }
